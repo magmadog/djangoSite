a8c02d0346c749c56f1650e5610d89823a63be22
magmadog <ya.magmarox@yandex.ru>
2020-07-11 16:58:48 +0300

    initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Артур Сарбаев\Desktop\Проекты\Python\magmadog\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
